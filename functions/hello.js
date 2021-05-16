a535642e39885217521b21c0a8dec916910a23cc
Guillermo Ortiz <guillermo.ortiz@wizeline.com>
2021-05-15 21:12:30 -0500

    only commits

--- functions/hello.js
+++ functions/hello.js
@@ -1,35 +1,22 @@
 const github = require('octonode');
 
 exports.handler = async (event) => {
-  const client = github.client(process.env.GH_TOKEN);
-  const repoR = await client.repo(`wizeline/patio-ui`);
+  try {
+    const client = github.client(process.env.GH_TOKEN);
+    const repoR = await client.repo(`wizeline/patio-ui`);
 
-  Promise.allSettled([repoR.prsAsync(), repoR.commitsAsync()])
-    .then((results) => {
-      const outputData = { pullRequests: null, commits: null };
-      results
-        .filter((result) => result.status === 'fulfilled')
-        .forEach((result, idx) => {
-          const [data] = result.value;
-          if (idx === 0) {
-            //PRs request
-            outputData.pullRequests = data;
-          } else {
-            outputData.commits = data;
-          }
-        });
+    const [data] = await repoR.commitsAsync();
 
-      return {
-        data: outputData,
-        error: null,
-      };
-    })
-    .catch((error) => {
-      return {
-        statusCode: 500,
-        body: {
-          error,
-        },
-      };
-    });
+    return {
+      statusCode: 500,
+      body: {
+        error,
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: { error },
+    };
+  }
 };
