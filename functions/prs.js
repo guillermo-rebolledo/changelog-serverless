138ac45cbbec8bf03cbd38dffa5f2edb194443af
Guillermo Ortiz <guillermo.ortiz@wizeline.com>
2021-05-15 21:20:23 -0500

    Modified routes and added PRs function

---
+++ functions/prs.js
@@ -0,0 +1,27 @@
+const github = require('octonode');
+
+exports.handler = async (event) => {
+  try {
+    const client = github.client(process.env.GH_TOKEN);
+    const repoR = await client.repo(`wizeline/patio-ui`);
+
+    const [data] = await repoR.prsAsync();
+
+    const body = {
+      error: null,
+      data,
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error,
+      }),
+    };
+  }
+};
