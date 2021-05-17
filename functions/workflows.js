ba7f7681d190bc7a8d60d2bafefb50135cbbe6da
Guillermo Ortiz <guillermo.ortiz@wizeline.com>
2021-05-16 21:17:01 -0500

    Added workflows endpoint

---
+++ functions/workflows.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+};
+
+exports.handler = async (event) => {
+  try {
+    const response = await fetch(
+      'https://api.github.com/repos/wizeline/patio-ui/actions/runs',
+      {
+        method: 'get',
+        headers: {
+          Authorization: `token ${process.env.GH_TOKEN}`,
+        },
+      },
+    );
+
+    const data = await response.json();
+
+    const body = {
+      error: null,
+      data,
+    };
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(body),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({
+        error,
+      }),
+    };
+  }
+};
